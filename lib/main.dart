cf797a031f278a23ca4d528ed3ede99567a0790c
CsAdi <oradam86@gmail.com>
2022-09-08 11:42:15 +0200

    FlashChat

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flash Chat',
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
